1c60f493cbb174e9fb7b3c5b3fc8346f7290a0eb
InquisidorIA <speedvort@gmail.com>
2026-07-10 17:15:37 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,2 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-  // El service worker simplemente existe para cumplir el requisito de instalación
-});
+self.addEventListener('install', (event) => { self.skipWaiting(); });
+self.addEventListener('fetch', (event) => {});
